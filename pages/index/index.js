d9500c4541b01bf0bda3204720e180fdb8133fca
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-10 20:25:21 +0800

    index map completed - data linked to api

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,43 +2,43 @@ Page({
   data: {
     latitude: 31.223520,
     longitude: 121.455909,
-    markers: [{
-      id: 1,
-      latitude: 31.220120,
-      longitude: 121.443128,
-      iconPath: '/image/pin.png',
-      // name: 'T.I.T 创意园'
-    },
-      {
-        id: 2,
-        latitude: 31.219166,
-        longitude: 121.443466,
-        iconPath: '/image/pin.png',
-        // name: 'T.I.T 创意园'
-      },
-      {
-        id: 3,
-        latitude: 31.227466,
-        longitude: 121.463488,
-        iconPath: '/image/pin.png',
-        // name: 'T.I.T 创意园'
-      },
-      {
-        id: 4,
-        latitude: 31.222989,
-        longitude: 121.451300,
-        iconPath: '/image/pin.png',
-        // name: 'T.I.T 创意园'
-      }],
-    covers: [{
-      latitude: 23.099994,
-      longitude: 113.344520,
-      // iconPath: '/image/pin.jpg'
-    }, {
-      latitude: 23.099994,
-      longitude: 113.304520,
-      // iconPath: '/image/pin.jpg'
-    }]
+    // markers: [{
+    //   id: 1,
+    //   latitude: 31.220120,
+    //   longitude: 121.443128,
+    //   iconPath: '/image/pin.png',
+    //   // name: 'T.I.T 创意园'
+    // },
+    //   {
+    //     id: 2,
+    //     latitude: 31.219166,
+    //     longitude: 121.443466,
+    //     iconPath: '/image/pin.png',
+    //     // name: 'T.I.T 创意园'
+    //   },
+    //   {
+    //     id: 3,
+    //     latitude: 31.227466,
+    //     longitude: 121.463488,
+    //     iconPath: '/image/pin.png',
+    //     // name: 'T.I.T 创意园'
+    //   },
+    //   {
+    //     id: 4,
+    //     latitude: 31.222989,
+    //     longitude: 121.451300,
+    //     iconPath: '/image/pin.png',
+    //     // name: 'T.I.T 创意园'
+    //   }],
+    // covers: [{
+    //   latitude: 23.099994,
+    //   longitude: 113.344520,
+    //   // iconPath: '/image/pin.jpg'
+    // }, {
+    //   latitude: 23.099994,
+    //   longitude: 113.304520,
+    //   // iconPath: '/image/pin.jpg'
+    // }]
   },
   onReady: function (e) {
     this.mapCtx = wx.createMapContext('myMap')
@@ -80,25 +80,27 @@ Page({
       }]
     })
   },
-  // onLoad: function (options) {
-  //   let page = this;
-  //   wx.request({
-  //     url: "https://easy-mock.com/mock/5acc974ffbaa0f54f4d6a502/stations",
-  //     method: 'GET',
-  //     success(res) {
-  //       const stations = res.data.stations;
-  //       const markers = [{
-  //         id: stations.id,
-  //         latitude: stations.latitude,
-  //         longitude: stations.longitude,
-  //         iconPath: '/image/pin.png',
-  //       }]
-  //     },
+  onLoad: function (options) {
+    let page = this;
+    wx.request({
+      url: "https://easy-mock.com/mock/5acc974ffbaa0f54f4d6a502/stations",
+      method: 'GET',
+      success(res) {
+        const stations = res.data.stations;
+        const markers = []
 
-  //       page.setData({
-  //         markers: markers
-  //       });
-  //       wx.hideToast();
-  //     }
-  // }
+        stations.map(station => {
+          const mark = {}
+            mark.id= station.id,
+            // console.log(mark.id)
+            mark.latitude= station.latitude,
+            mark.longitude = station.longitude,
+            mark.iconPath = '/image/pin.png'
+            markers.push(mark)
+        })
+        page.setData({markers: markers})
+        console.log(page.data.markers)
+      }
+    })
+  },
 })
