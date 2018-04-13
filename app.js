f9f9b2814b29cdaa497a61609f825742e6a5e1e1
Pavel Liser <liser.pavel@gmail.com>
2018-04-13 14:08:30 +0800

    made app.js login configured to get wechat user info

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ App({
     // 登录
     wx.login({
       success: function (res) {
+        
         if (res.code) {
           
           //发起网络请求
@@ -26,6 +27,34 @@ App({
               // console.log(res.data)
               wx.setStorageSync('openid', res.data.openid)
               wx.setStorageSync('user_id', res.data.id)
+              wx.getSetting({
+                success: res => {
+                  console.log(res)
+                  
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                      success: res => {
+                        console.log(res)
+                        // 可以将 res 发送给后台解码出 unionId
+                        page.globalData.userInfo = res.userInfo
+
+                        // wx.request({
+                        //   url: 'http://localhost:3000/api/v1/users/',
+                        //   method: "POST",
+                        //   data: {
+                        //     name: res.userInfo.nickName}
+                        //   });
+
+                        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                        // 所以此处加入 callback 以防止这种情况
+                        if (this.userInfoReadyCallback) {
+                          this.userInfoReadyCallback(res)
+                        }
+                      }
+                    })
+                  }
+                
+              })
               // page.globalData,setData({})
             }
           })
@@ -35,33 +64,7 @@ App({
       }
     });
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log(res)
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              
-              // wx.request({
-              //   url: 'http://localhost:3000/api/v1/users/',
-              //   method: "POST",
-              //   data: {
-              //     name: res.userInfo.nickName}
-              //   });
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    
   },
   globalData: {
     userInfo: null
