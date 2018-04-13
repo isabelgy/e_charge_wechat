a3e5bca9fc237bee3c8690c46a32f8a8550cf562
Wendy Lee <wanderlustwendy@icloud.com>
2018-04-13 13:52:49 +0800

    added history stack

--- pages/show/show.js
+++ pages/show/show.js
@@ -6,6 +6,7 @@ Page({
    */
   data: {
     qr_result: "",
+    address: ""
   },
 
   scan: function (e) {
@@ -18,10 +19,12 @@ Page({
       onlyFromCamera: true,
       success: (res) => {
         wx.request({
-          url: `http://localhost:3000/api/v1/stations/${page.data.id}/rentals`,
-          // url: `http://e-charge.herokuapp.com/api/v1/stations/${page.data.id}/rentals`,
+
+          //url: `http://localhost:3000/api/v1/stations/${page.data.id}/rentals`,
+
+          url: `http://e-charge.herokuapp.com/api/v1/stations/${page.data.id}/rentals`,
           method: 'POST',
-          data: {user_id: (wx.getStorageSync('user_id')), station_id: page.data.id},
+          data: {user_id: (wx.getStorageSync('user_id')), station_id:   page.data.id},
           success: function (res) {
             console.log(res.data)
             wx.navigateTo({
@@ -55,20 +58,58 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    
-    // loading specific station data from localhost
+    let page = this;
+    // loading specific station data from api
     const id = options.id
-    const page = this
+    
     wx.request({
-      url: `http://localhost:3000/api/v1/stations/${id}`,
+//       url: `http://localhost:3000/api/v1/stations/${id}`,
+      url: `http://e-charge.herokuapp.com/api/v1/stations/${id}`,
       success: function (res) {
         page.setData(res.data);
-        
       }
     })
   },
 
+  editStation: function (e) {
+    console.log(e)
+    let page = this;
+    const id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `/pages/edit/edit?id=${id}`,
+    })
+  },
+
+  showMap: function (e) {
+    // console.log(e)
+    const lat = Number(e.currentTarget.dataset.id[0])
+    const long = Number(e.currentTarget.dataset.id[1])
+    const address = e.currentTarget.dataset.id[2]
+    let page = this;
+    // wx.getLocation({
+    //   type: 'gcj02',
+    //   sucess: function(res){
+    //     wx.openLocation({
+    //       latitude: lat,
+    //       longitude: long,
+    //       scale: 28,
+    //       address: address
+    //   })
+    //     console.log(e)
+    // }
+    // })
+    
+    wx.openLocation({
+      latitude: lat,
+      longitude: long,
+      scale: 28,
+      address: address 
+    })
+    // wx.navigateBack({
+
+  
+    },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -116,5 +157,7 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  
 })
